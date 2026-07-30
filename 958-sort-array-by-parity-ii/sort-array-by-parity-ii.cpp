74a4f218bdcb44c40b8a5d169bba9b136719bf3e
abhishek07y <btech15108.24@bitmesra.ac.in>
2026-07-30 22:18:13 +0530

    Time: 0 ms (100.00%) | Memory: 25.8 MB (76.58%) - LeetSync

---
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int even=0;
+        int odd=1;
+        vector<int> ans(nums.size());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0){
+                ans[even]=nums[i];
+                even=even+2;
+            }
+            else{
+                ans[odd]=nums[i];
+                odd=odd+2;
+            }
+        }
+        return ans;
+
+        
+    }
+};
