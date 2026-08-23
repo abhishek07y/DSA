b0bb2528057661ce02582a2e256edbed2694b01e
abhishek07y <btech15108.24@bitmesra.ac.in>
2026-08-23 19:34:32 +0530

    Time: 5 ms (30.77%) | Memory: 13.9 MB (67.83%) - LeetSync

---
+++ 2039-sum-game/sum-game.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int leftsum=0;
+        int rightsum=0;
+        int leftq=0;
+        int rightq=0;
+        for(int i=0 ;i<n/2 ;i++){
+            if(num[i]=='?'){
+                leftq++;
+
+
+            }else{
+                leftsum+=num[i]-'0';
+            }
+        }
+        for(int i=n/2 ;i<n ;i++){
+            if(num[i]=='?'){
+                rightq++;
+
+
+            }else{
+                rightsum+=num[i]-'0';
+            }
+           
+
+        }
+        if((rightq+leftq)%2==1){
+            return true;
+        }
+        if((leftsum-rightsum) != 9*(rightq-leftq)/2) return true;
+        return false;
+    }
+};
