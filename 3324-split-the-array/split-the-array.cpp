9cdedacde48d03248657c629a1f46083a86a462a
abhishek07y <btech15108.24@bitmesra.ac.in>
2026-09-02 18:30:57 +0530

    Time: 0 ms (100.00%) | Memory: 25.3 MB (11.10%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+        unordered_map<int ,int>mpp;
+        for(auto x : nums) {
+            mpp[x]++;
+        }
+        for(auto it : mpp) {
+            if(it.second > 2) {
+            return false;
+            }
+        
+        }
+        return true;
+        
+    }
+};
